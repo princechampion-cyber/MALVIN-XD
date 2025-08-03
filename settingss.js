41701f2d131c99ee18186e2ec2df87aea5b84ff8
princechampion-cyber <princechampion888@gmail.com>
2025-08-03 22:40:22 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~tisAXL4I#gEgfdGWeJpmvN_U5bB2psxYG1L-24GBWQK0JVOBJrRc",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2347049832297", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
